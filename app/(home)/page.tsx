63996dc8c99920c7484fb28fbdb61bedf6936d64
Ashutosh Kumar <132209941+AshutoshStark@users.noreply.github.com>
2025-08-20 23:45:16 +0530

    update

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,12 +3,10 @@
 import {
   NavigationMenu,
   NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 import Link from "next/link";
 import { useState } from "react";
